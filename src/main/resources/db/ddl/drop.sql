7987d7d41333fda3a34477a32d50c357089318b1
Stefani, Massimo <massimo.cruz25@gmail.com>
2024-08-20 09:31:15 +0200

    feat: sql for the moment

---
+++ src/main/resources/db/ddl/drop.sql
@@ -0,0 +1,16 @@
+    drop table if exists api_user cascade;
+    drop table if exists person cascade;
+    drop table if exists teacher cascade;
+    drop table if exists student cascade;
+    drop table if exists discipline cascade;
+    drop table if exists course cascade;
+    drop table if exists class cascade;
+    drop table if exists class_session cascade;
+    drop table if exists room cascade;
+    drop table if exists site cascade;
+    drop table if exists loan cascade;
+    drop table if exists returned_loan cascade;
+    drop table if exists presence_box cascade;
+    drop table if exists presence cascade;
+    drop table if exists class_student cascade;
+
