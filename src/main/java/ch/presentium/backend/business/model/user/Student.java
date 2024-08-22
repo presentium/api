4f9814e77e6ef60d822c21469932402b2b9e11f8
Massimo Stefani <massimo.cruz25@gmail.com>
2024-08-22 22:48:28 +0200

    feat: student api

--- src/main/java/ch/presentium/backend/business/model/user/Student.java
+++ src/main/java/ch/presentium/backend/business/model/user/Student.java
@@ -4,7 +4,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.ForeignKey;
 import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
