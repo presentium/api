fc1e66dff6ddcdd5b4444adaaf0d0c0923d08d01
Massimo Stefani <massimo.cruz25@gmail.com>
2024-08-22 22:47:54 +0200

    feat: student api

--- src/main/java/ch/presentium/backend/api/student/StudentController.java
+++ src/main/java/ch/presentium/backend/api/student/StudentController.java
@@ -1,6 +1,5 @@
 package ch.presentium.backend.api.student;
 
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.transaction.annotation.Transactional;
